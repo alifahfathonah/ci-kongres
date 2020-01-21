8b81f1b97b7bb0bc1c06b64b7834b45daf3407c4
irfanjoelax <irfanthejoelax@gmail.com>
2020-01-21 13:42:29 +0800

    v.1.4 - perubahan front end download TOR

--- application/views/front/v_tor.php
+++ application/views/front/v_tor.php
@@ -3,34 +3,19 @@
    <h2>T O R</h2>
 </div>
 <hr>
-<div class="badge badge-primary text-wrap" style="width: 6rem;">
+<div class="badge badge-danger text-wrap" style="width: 6rem;">
    Catatan
 </div>
 <p class="text-muted text-justify">TOR dapat berubah sewaktu-waktu, menyesuaikan situasi dan kondisi berlangsungnya kongres</p>
 <div class="row mt-5">
    <div class="col text-center">
-      <button type="button" onclick="torDownload" class="btn btn-lg btn-outline-primary">
+      <a href="<?= base_url('upload/tor/' . $tor->file_tor) ?>" class="btn btn-lg btn-outline-primary">
          <i class="fa fa-download"></i> &nbsp; Download Disini
-      </button>
+      </a>
    </div>
 </div>
 
 <?php $this->load->view('front/layouts/footer'); ?>
-<script>
-   function torDownload() {
-      $.ajax({
-         type: "GET",
-         url: "<?= base_url('upload/tor/' . $tor->file_tor) ?>",
-         beforeSend: function() {
-            $(".se-pre-con").fadeIn("slow");
-         },
-         success: function(response) {
-            window.history.back();
-            $(".se-pre-con").fadeOut("slow");
-         }
-      });
-   }
-</script>
 </body>
 
 </html>
