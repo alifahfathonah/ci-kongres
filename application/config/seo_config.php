1d3f7d665269020c0060aa334c7cb6d3de203ed1
irfanjoelax <irfanthejoelax@gmail.com>
2020-01-27 10:05:38 +0800

    v.1.9 - penambahan helpers SEO CI

---
+++ application/config/seo_config.php
@@ -0,0 +1,5 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['seo_title'] = 'Kongres PMII XX';
+$config['seo_desc'] = 'Kongres Nasional PMII Ke-XX Balikpapan';
+$config['seo_imgurl'] = base_url('asset/favicon.png');
